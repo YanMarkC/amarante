982aaf4d0cc41b773ebd68cb4b85b955efe4c71d
YanMarkC <amaranteian4@gmail.com>
2026-05-13 10:54:02 +0800

    Renamed app and updated package name

--- android/app/src/main/java/com/ianmarka/daybreakapp/MainActivity.kt
+++ android/app/src/main/java/com/ianmarka/daybreakapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.freshapp
+package com.ianmarka.daybreakapp;
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/ianmarka/daybreakapp/MainApplication.kt
+++ android/app/src/main/java/com/ianmarka/daybreakapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.freshapp
+package com.ianmarka.daybreakapp;
 
 import android.app.Application
 import com.facebook.react.PackageList
